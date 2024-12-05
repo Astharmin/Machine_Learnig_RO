7fe439da2618145d25fe66cd1e470456feef311d
Aaron Barcenas <114181941+Astharmin@users.noreply.github.com>
2024-12-05 15:25:28 -0400

    agregando modulo de control de imagenes

---
+++ src/app/components/image-control/image-control.component.ts
@@ -0,0 +1,17 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-image-control',
+  imports: [NgIf],
+  templateUrl: './image-control.component.html',
+  styleUrl: './image-control.component.css'
+})
+export class ImageControlComponent {
+  imageUrl: string | null = null;
+  selectedFile: File | null = null;
+  //prediction$: Observable < any[] | null>;
+  //loading$: Observable <boolean>;
+  //error$: Observable <string | null>;
+}
