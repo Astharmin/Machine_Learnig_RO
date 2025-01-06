3f949f3ef496d9b1299f9ee848e1282291b79558
Aaron Barcenas <114181941+Astharmin@users.noreply.github.com>
2025-01-06 17:40:37 -0400

    Actualizacion del endpoint para poner modelo

--- src/app/components/image-control/image-control.component.ts
+++ src/app/components/image-control/image-control.component.ts
@@ -1,17 +1,26 @@
-import { NgIf } from '@angular/common';
+import { AsyncPipe, NgIf, PercentPipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Prediction } from '../../types/types';
+import { UploadService } from '../../services/upload.service';
 
 @Component({
   selector: 'app-image-control',
-  imports: [],
+  standalone: true,
+  imports: [AsyncPipe, UpperCasePipe, PercentPipe],
   templateUrl: './image-control.component.html',
   styleUrl: './image-control.component.css'
 })
 export class ImageControlComponent {
   imageUrl: string | null = null;
   selectedFile: File | null = null;
-  //prediction$: Observable < any[] | null>;
-  //loading$: Observable <boolean>;
-  //error$: Observable <string | null>;
+  prediction$: Observable < Prediction[] | null>;
+  loading$: Observable <boolean>;
+  error$: Observable <string | null>;
+
+  constructor(private uploadImageService: UploadService){
+    this.prediction$ = this.uploadImageService.prediction$
+    this.loading$ = this.uploadImageService.loading$
+    this.error$ = this.uploadImageService.error$
+  }
 }
---
+++ src/app/services/upload.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, catchError, Observable, tap, throwError } from 'rxjs';
+import { Prediction } from '../types/types';
+import { HttpClient, HttpErrorResponse} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadService {
+  private predictionSubject = new BehaviorSubject <Prediction[] | null> (null)
+  private loadingSubject = new BehaviorSubject <boolean>(false)
+  private errorSubject = new BehaviorSubject <string | null>(null)
+  
+
+  prediction$ = this.predictionSubject.asObservable()
+  loading$ = this.loadingSubject.asObservable()
+  error$ = this.errorSubject.asObservable()
+
+  constructor(private http: HttpClient) { }
+
+  uploadImage(file: File, uploadUrl: string): Observable<Prediction[]> {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    this.loadingSubject.next(true);
+    this.errorSubject.next(null);
+
+    return this.http.post<Prediction[]>(uploadUrl, formData).pipe(
+      tap((predictions) => {
+        this.predictionSubject.next(predictions);
+        this.loadingSubject.next(false)
+      }),
+      catchError(this.handleError)
+    )
+  }
+  private handleError = (error: HttpErrorResponse): Observable<never> => {
+    this.loadingSubject.next(false);
+    let errorMessage = 'An error occurred';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.error.message}`;
+    }
+    else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    this.errorSubject.next(errorMessage)
+    return throwError(() => new Error(errorMessage))
+  }
+}
