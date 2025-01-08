188ff3c3b6a1a1391ae9a9aeb8980d762423779e
Aaron Barcenas <114181941+Astharmin@users.noreply.github.com>
2025-01-08 16:43:00 -0400

    arreglos de codificación en EndPoint

---
+++ src/app/components/image/image.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-image',
+  imports: [],
+  templateUrl: './image.component.html',
+  styleUrl: './image.component.css'
+})
+export class ImageComponent {
+@Input({required: true}) imageUrl: string = '';
+}
---
+++ src/app/components/prediction/prediction.component.ts
@@ -0,0 +1,13 @@
+import { PercentPipe, UpperCasePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Prediction } from '../../types/types';
+
+@Component({
+  selector: 'app-prediction',
+  imports: [UpperCasePipe, PercentPipe],
+  templateUrl: './prediction.component.html',
+  styleUrl: './prediction.component.css'
+})
+export class PredictionComponent {
+@Input({required: true}) prediction: Prediction | null = null;
+}
